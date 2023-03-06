c14cf057818f50c34ff9a75e5328c00e97fa3309
miralaba <miralaba@gmail.com>
2023-03-06 15:01:40 +0000

    updated script

--- scripts/scienciometric.R
+++ scripts/scienciometric.R
@@ -18,6 +18,12 @@ head(concession.publication.raw.data)
 str(concession.publication.raw.data)
 length(unique(concession.publication.raw.data$ID))
 
+#adjustments (replacing neotropical by neotropics, aves by birds and trees* by trees)
+concession.publication.raw.data$Region <- ifelse(concession.publication.raw.data$Region == "Netropical", "Neotropics", concession.publication.raw.data$Region)
+concession.publication.raw.data$Topic <- ifelse(concession.publication.raw.data$Topic == "Aves", "Birds", concession.publication.raw.data$Topic)
+concession.publication.raw.data$Topic <- ifelse(concession.publication.raw.data$Topic == "Trees*", "Trees", concession.publication.raw.data$Topic)
+
+
 #excluding 2022 -- searches were conducted in June 2022
 concession.publication.raw.data %>% distinct(ID, .keep_all = T) %>% filter(Year < 2022)
 
@@ -130,6 +136,7 @@ NStudy.byTopic.df <- concession.publication.data.stay %>%
                          ungroup() %>% 
                          arrange(desc(`n()`))
 
+write.csv(NStudy.byTopic.df, "results/N_study_bytopic.csv", row.names = F)
 
 
 g3 <- concession.publication.data.stay %>% filter(Region != "NA" & Year < 2022) %>%
@@ -137,7 +144,8 @@ g3 <- concession.publication.data.stay %>% filter(Region != "NA" & Year < 2022) 
                                                  count(Topic) %>%
                                               ungroup() %>%
                                                  ggplot(aes(x=reorder(Topic, n), y=n, fill=Region))+
-                                                    geom_bar(stat = "identity")+
+                                                    geom_bar(stat = "identity") +
+                                                    geom_hline(yintercept = 20, linetype = "dashed", linewidth = 1, color ="gray56") + 
                                                     scale_fill_manual(values = c("#cbb466", "#cfaec6", "#b38c7e", "#aecfb7", "#8c7eb3"))+
                                                     coord_flip()+
                                                     labs(x="Topics", y="N. publications") +
@@ -161,6 +169,49 @@ dev.off()
 
 
 
+#study gap evidence
+region.interest <- c("Neotropics", "Africa", "Insular Asia", "Continental Asia")
+topic.interest <- c("Forest structure/damage/biomass", "Exploited tree species", "Seed dispersal", "Logging and bushmeat", "Impacts on non-timbers", "Amphibians")
+
+g4 <- concession.publication.data.stay %>% 
+                     select(Region, `Logging events`, `Time effect`, Intensity, Topic) %>% 
+                     filter(Region %in% region.interest) %>%
+                     filter(Topic %in% topic.interest) %>% 
+                     replace(. == "Insular Asia" | . == "Continental Asia", "Asia") %>%
+                     replace(is.na(.), "unknown") %>%
+                     group_by(Region, `Logging events`, `Time effect`, Intensity, Topic) %>% 
+                     tally() %>% 
+                     ungroup() %>% 
+                     mutate(`Time effect` = factor(`Time effect`, levels = c("<=10", "10-30", "30-50", ">50", "unknown")),
+                            `Logging events` = factor(`Logging events`, levels = c(">2", "2", "1", "unknown")),
+                            Intensity = factor(Intensity, levels = c("low", "medium", "high", "unknown"))) %>% 
+                     ggplot(aes(`Time effect`, Topic, color = Intensity, alpha=.5, size=n)) +
+                     geom_point(position = position_dodge(.3)) +
+                     scale_color_manual(values = c("#07b6f8", "#f8c207", "#f8073d", "#97969b")) +
+                     scale_size_continuous(breaks = c(1,5,10,15,20), range = c(3,10)) +
+                     labs(x="", y="") +
+                     guides(alpha = "none", size = guide_legend("#N. Studies"), color = guide_legend(override.aes = list(shape=15, size = 7))) +
+                                        facet_grid(`Logging events`~Region) +
+                     theme(axis.title = element_text(family = "serif", size = 22),
+                           axis.text.x = element_text(family = "serif", size = 18, angle = 45, hjust = 1),
+                           axis.text.y = element_text(family = "serif", size = 18, vjust = .5),
+                           axis.line.y = element_line(linewidth = 1), axis.line.x = element_line(linewidth = 1),
+                           axis.ticks.y = element_line(linewidth = 1), axis.ticks.x = element_line(linewidth = 1),
+                           legend.title = element_text(family = "serif", size = 18),
+                           legend.text = element_text(family = "serif", size = 18),
+                           panel.background = element_blank(),
+                           panel.spacing.x = unit(1, "lines"),
+                           panel.spacing.y = unit(2, "lines"),
+                           strip.text = element_text(family = "serif", size = 18),
+                           legend.key = element_rect(fill = "transparent"))
+                     
+
+
+png("results/study_gap.png", width = 1024, height = 750, units = "px")
+g4
+dev.off()
+
+
 
 
 #graph effect time since logging
@@ -174,7 +225,7 @@ NStudy.byTimeeffect.df <- concession.publication.data.stay %>%
 
 
 
-g4 <- concession.publication.data.stay %>% distinct(ID, .keep_all = T) %>% 
+g5 <- concession.publication.data.stay %>% distinct(ID, .keep_all = T) %>% 
                                              group_by(Region) %>%
                                                filter(Region != "NA" & Year < 2022) %>%
                                                count(`Time effect`) %>%
@@ -182,6 +233,7 @@ g4 <- concession.publication.data.stay %>% distinct(ID, .keep_all = T) %>%
                                                mutate(`Time effect` = factor(`Time effect`, levels = c(">50", "30-50", "10-30", "<=10"))) %>% 
                                                  ggplot(aes(x=`Time effect`, y=n, fill=Region))+
                                                    geom_bar(stat = "identity")+
+                                                   geom_hline(yintercept = 20, linetype = "dashed", linewidth = 1, color ="gray56") + 
                                                    scale_fill_manual(values = c("#cbb466", "#cfaec6", "#b38c7e", "#aecfb7", "#8c7eb3"))+
                                                    coord_flip(ylim = c(0, 500)) +
                                                    theme(legend.position = c(0.9,0.4), legend.background = element_blank())+
@@ -219,7 +271,7 @@ NStudy.byNevents.df <- concession.publication.data.stay %>%
 
 
 
-g5 <- concession.publication.data.stay %>% distinct(ID, .keep_all = T) %>% 
+g6 <- concession.publication.data.stay %>% distinct(ID, .keep_all = T) %>% 
                                             group_by(Region) %>%
                                                filter(Region != "NA" & Year < 2022) %>%
                                                count(`Logging events`) %>%
@@ -227,6 +279,7 @@ g5 <- concession.publication.data.stay %>% distinct(ID, .keep_all = T) %>%
                                                mutate(`Logging events` = factor(`Logging events`, levels = c(">2", "2", "1"))) %>% 
                                                   ggplot(aes(x=`Logging events`, y=n, fill=Region))+
                                                      geom_bar(stat = "identity")+
+                                                     geom_hline(yintercept = 20, linetype = "dashed", linewidth = 1, color ="gray56") + 
                                                      scale_fill_manual(values = c("#cbb466", "#cfaec6", "#b38c7e", "#aecfb7", "#8c7eb3"))+
                                                      coord_flip(ylim = c(0, 500))+
                                                      theme(legend.position = c(0.9,0.4), legend.background = element_blank())+
@@ -264,7 +317,7 @@ NStudy.byIntensity.df <- concession.publication.data.stay %>%
 
 
 
-g6 <- concession.publication.data.stay %>% distinct(ID, .keep_all = T) %>% 
+g7 <- concession.publication.data.stay %>% distinct(ID, .keep_all = T) %>% 
                                             group_by(Region) %>%
                                                filter(Region != "NA" & Year < 2022) %>%
                                                count(Intensity) %>%
@@ -272,6 +325,7 @@ g6 <- concession.publication.data.stay %>% distinct(ID, .keep_all = T) %>%
                                                 mutate(Intensity = factor(Intensity, levels = c("high", "medium", "low"))) %>% 
                                                     ggplot(aes(x=Intensity, y=n, fill=Region))+
                                                        geom_bar(stat = "identity")+
+                                                       geom_hline(yintercept = 20, linetype = "dashed", linewidth = 1, color ="gray56") + 
                                                        scale_fill_manual(values = c("#cbb466", "#cfaec6", "#b38c7e", "#aecfb7", "#8c7eb3"))+
                                                        coord_flip(ylim = c(0, 500))+
                                                        theme(legend.position = c(0.9,0.4), legend.background = element_blank())+
@@ -283,7 +337,7 @@ g6 <- concession.publication.data.stay %>% distinct(ID, .keep_all = T) %>%
                                                              axis.ticks.y = element_line(linewidth = 1), axis.ticks.x = element_line(linewidth = 1),
                                                              legend.title = element_text(family = "serif", size = 18),
                                                              legend.text = element_text(family = "serif", size = 18),
-                                                             legend.position = "none",
+                                                             #legend.position = "none",
                                                              panel.background = element_blank(),
                                                              panel.spacing.x = unit(1, "lines"),
                                                              panel.spacing.y = unit(2, "lines"),
@@ -295,15 +349,19 @@ g6 <- concession.publication.data.stay %>% distinct(ID, .keep_all = T) %>%
 
 
 
-
-
-
-png("results/effects.png", width = 1440, height = 1080, units = "px", bg = "transparent")
+png("results/study_sites_characteristics.png", width = 920, height = 1080, units = "px", bg = "transparent")
 ggarrange(
-  g3, ncol = 2, nrow = 1, widths = c(2,1), labels = "(A)",
-  ggarrange(
-    g4, g5, g6, ncol = 1, nrow = 3, align = "hv", labels = c("(B)", "(C)", "(D)"), label.x = -.05
-  )
+    g5, g6, g7, ncol = 1, nrow = 3, align = "hv", labels = c("(A)", "(B)", "(C)")#, label.x = -.05
 )
 dev.off()
+
+
+#png("results/effects.png", width = 1440, height = 1080, units = "px", bg = "transparent")
+#ggarrange(
+#  g3, ncol = 2, nrow = 1, widths = c(2,1), labels = "(A)",
+#  ggarrange(
+#    g4, g5, g6, ncol = 1, nrow = 3, align = "hv", labels = c("(B)", "(C)", "(D)"), label.x = -.05
+#  )
+#)
+#dev.off()
 #
